f72fd3ff245e433960fe0c3f55f61df104f96838
Sonjoy Roy <sonjoy209164@gmail.com>
2025-07-12 18:38:42 +0600

    Completed the task

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\Api\TaskController;
+
+Route::post('/tasks', [TaskController::class, 'store']);
+Route::patch('/tasks/{id}', [TaskController::class, 'update']);
+Route::get('/tasks/pending', [TaskController::class, 'pending']);
+
+use App\Http\Controllers\PostController;
+
+Route::post('/posts', [PostController::class, 'store']);         // Create a post
+Route::get('/posts', [PostController::class, 'index']);          // List all posts
+Route::get('/posts/{id}', [PostController::class, 'show']);      // View single post
+
+use App\Http\Controllers\AuthController;
+
+Route::post('/register', [AuthController::class, 'register']);
